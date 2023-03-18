f65f2453fd1151471aa087e42c07865ef34891da
Mohammad <mohammad1162002@gmail.com>
2023-03-18 21:37:33 +0200

    Report disconts

--- Program.cs
+++ Program.cs
@@ -42,11 +42,6 @@ if (Console.ReadLine().ToLower().Equals("yes"))
             if (!double.TryParse(Console.ReadLine(), out DiscountValue))
                 throw new Exception("InValid discount Value");
             ListOfBooks = ListOfBooks.Select(c => { book.BookDiscount = DiscountValue; return c; }).ToList();
-
-           
-            Console.Write($"Book with name = {book.Name}, UPC = {book.UPC}, price ={book.Price} Tax = {Tax.TaxRate} %, ");
-            Console.Write($"discount = {book.BookDiscount} % Tax amount = {book.TaxAmount()} Discount amount = ${book.DiscountAmount()}");
-            Console.WriteLine($" Price before = ${book.Price}, price after = ${book.PriceAfterDiscount()}");
             break;
         }
         catch (Exception)
@@ -55,7 +50,13 @@ if (Console.ReadLine().ToLower().Equals("yes"))
             Console.WriteLine("Invalid Discount Value!");
         }
     }
+    ListOfBooks.ForEach(book => {
+        if (book.BookDiscount != 0)
+            Report.ReportDiscount(book.BookDiscount, book.PriceAfterTax(), book.DiscountAmount());
+        else
+            Report.ReportNoDiscount(book.PriceAfterTax());
+        });
 }
-    Console.WriteLine("Press Any key to exit...");
+    Console.WriteLine("Press Any key to exit..."); 
 Console.ReadLine();
 
