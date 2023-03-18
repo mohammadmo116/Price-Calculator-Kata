7e56c1f44d43fe0e58ce27172c500584085f637c
Mohammad <mohammad1162002@gmail.com>
2023-03-18 22:00:57 +0200

    d

---
+++ Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Calculator_Kata
+{
+    public static class Report
+    {
+        public static void ReportDiscount(double discount, double price, decimal discountAmount)
+        {
+            Console.WriteLine($"Tax = {Tax.TaxRate}, discount= {discount} price={price} , {discountAmount}$ was deduced");
+     
+        }
+        public static void ReportNoDiscount(double price)
+        {
+            Console.WriteLine($"Tax = {Tax.TaxRate}, discount= no discount , price={price}");
+
+        }
+    }
+    
+}
